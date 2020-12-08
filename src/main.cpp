52381659233563b4ff4e54bda1942abc5fc54bed
Keith <kthbarnes11@gmail.com>
2020-12-08 13:59:16 +0530

    mqtt + json implementation

--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,30 @@
 #include <ESP8266WiFi.h>
 #include "settingsPZEM.h"
 #include <ArduinoJson.h>
-
+#include <PubSubClient.h>
 
 #include <SoftwareSerial.h>  //  ( NODEMCU ESP8266 )
-SoftwareSerial pzem(14, 12); // (RX,TX) connect to TX,RX of PZEM for NodeMCU
+SoftwareSerial pzem(14, 12); // (RX,TX) connect to TX,RX of PZEM for NodeMCU | D5-> 14 | D6-> 12|
 //SoftwareSerial pzem(D7,D8);  // (RX,TX) connect to TX,RX of PZEM
 #include <ModbusMaster.h>
 ModbusMaster node;
 SimpleTimer timer;
 
 int timerTask1;
-double U_PR, I_PR, P_PR, PPR, PR_F, PR_PF, PR_alarm;
+double U_PR, I_PR, P_PR, PPR, PR_F, PR_PF, PR_alarm; // PZEM registers
 uint8_t result;
 uint16_t data[6];
+
+const char *ssid = "BARNES";
+const char *password = "123MISS456";
+
+const char *mqttServer = "35.192.157.47"; 
+const int mqttPort = 1883;
+const char *mqttUser = "";
+const char *mqttPassword = "";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
 
 void setup()
 {
@@ -26,17 +37,46 @@ void setup()
   Serial.println("Start PZEM serial");
   node.begin(1, pzem);
   Serial.println("Start PZEM"); // 1 = ID MODBUS
+
+  WiFi.begin(ssid, password);
+
+
+  // wifi connection
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.println("Connecting to WiFi..");
+  }
+  Serial.println("Connected to the WiFi network");
+
+  client.setServer(mqttServer, mqttPort);
+
+
+  // mqtt connection
+  while (!client.connected())
+  {
+    Serial.println("Connecting to MQTT...");
+
+    if (client.connect("ESP32Client", mqttUser, mqttPassword))
+    {
+
+      Serial.println("connected");
+    }
+    else
+    {
+
+      Serial.print("failed with state ");
+      Serial.print(client.state());
+      delay(2000);
+    }
+  }
+
   
-
-
-  
-
-  //  timerTask1 = timer.setInterval(1000, updateBlynk);
 }
 
 void loop()
 {
-
+  // read calulated values from PZEM
   result = node.readInputRegisters(0x0000, 10);
   if (result == node.ku8MBSuccess)
   {
@@ -48,47 +88,59 @@ void loop()
     PR_PF = (node.getResponseBuffer(0x08) / 100.0f);
     PR_alarm = (node.getResponseBuffer(0x09));
   }
-
+  
+  // print values on serial monitor
   Serial.print("Voltage: ");
   Serial.print(U_PR); // V
   Serial.println(" V");
 
-
   Serial.print("Current: ");
   Serial.print(I_PR, 3); //  A
   Serial.println(" A");
-  
+
   Serial.print("Power: ");
   Serial.print(P_PR); //  W
   Serial.println(" W");
-  
+
   Serial.print("Energy: ");
   Serial.print(PPR, 3); // kWh
   Serial.println(" KWh");
-  
-  
+
   Serial.print("Frequency: ");
   Serial.print(PR_F); // Hz
   Serial.println(" Hz");
-  
+
   Serial.print("Power Factor: ");
   Serial.println(PR_PF);
-  
+
   Serial.println("====================================================");
-
-  StaticJsonDocument<200> doc;
   
-  doc["Voltage"] = U_PR;
-  doc["Current"] = I_PR;
-  doc["Power"] = P_PR;
-  doc["Energy"] = PPR;
-  doc["Frequency"] = PR_F;
+  // convert values to JSON format so that we can send a single json file to the broker
 
-  serializeJsonPretty(doc, Serial);
+  StaticJsonDocument<200> JSONbuffer;
 
-  
+  JSONbuffer["Voltage"] = U_PR;
+  JSONbuffer["Current"] = I_PR;
+  JSONbuffer["Power"] = P_PR;
+  JSONbuffer["Energy"] = PPR;
+  JSONbuffer["Frequency"] = PR_F;
 
+  serializeJsonPretty(JSONbuffer, Serial); // print json on serial monitor
+  Serial.println("");
 
+  // creating a string - buffer , since only string can be published to mqtt
+  char buffer[256];
+  serializeJson(JSONbuffer, buffer);
+  client.publish("esp/pzem", buffer);
+
+  if (client.publish("esp/pzem", buffer) == true)
+  {
+    Serial.println("Success sending message");
+  }
+  else
+  {
+    Serial.println("Error sending message");
+  }
 
   delay(1000);
 }
